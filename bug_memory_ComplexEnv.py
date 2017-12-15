bdeb93fe7861db6530ed67307b30769865b254f9
brandynwong <b7wong@ucsd.edu>
2017-12-15 00:57:01 -0500

    updated score adjustments to all be 100

--- bug_memory_ComplexEnv.py
+++ bug_memory_ComplexEnv.py
@@ -3,20 +3,20 @@
 # Hash table for bug memory, with each food/predator has its own alpha and beta values
 bug_memory = {}
 # predator [alpha, beta, survival, expected alpha, expected beta]
-bug_memory['spider'] = [0, 1, -100, 1, 0]
+bug_memory['spider'] = [0.49, 0.51, -100, 1, 0]
 # bug_memory['spider'] = [0.49, 0.51, -100, 1, 0]
-bug_memory['bird'] = [0.49, 0.51, -50, 1, 0]
+bug_memory['bird'] = [0.49, 0.51, -100, 1, 0]
 bug_memory['bat'] = [0.49, 0.51, -100, 1, 0]
-bug_memory['snake'] = [0.49, 0.51, -20, 1, 0]
-bug_memory['chicken'] = [0.49, 0.51, -50, 1, 0]
-bug_memory['duck'] = [0.49, 0.51, -50, 1, 0]
+bug_memory['snake'] = [0.49, 0.51, -100, 1, 0]
+bug_memory['chicken'] = [0.49, 0.51, -100, 1, 0]
+bug_memory['duck'] = [0.49, 0.51, -100, 1, 0]
 
 # food
-bug_memory['sunflower'] = [1, 0, 100, 0, 1]
+bug_memory['sunflower'] = [0.49, 0.51, 100, 0, 1]
 bug_memory['strawberry'] = [0.49, 0.51, 100, 0, 1]
-bug_memory['rose'] = [0.49, 0.51, 20, 0, 1]
-bug_memory['leaf'] = [1, 0, 20, 0, 1]
-bug_memory['orange'] = [0.49, 0.51, 10, 0, 1]
+bug_memory['rose'] = [0.49, 0.51, 100, 0, 1]
+bug_memory['leaf'] = [0.49, 0.51, 100, 0, 1]
+bug_memory['orange'] = [0.49, 0.51, 100, 0, 1]
 
 def getValue(label):
     return bug_memory[label]
